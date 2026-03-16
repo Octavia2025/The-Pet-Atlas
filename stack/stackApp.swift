4aa3ca56b1eabb55c45d289fa5adfc53028fd260
Octavia2025 <octaviatmucheche@gmail.com>
2026-03-16 16:20:31 +0200

    Initial Commit

---
+++ stack/stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackApp.swift
+//  stack
+//
+//  Created by Octavia Mucheche on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
